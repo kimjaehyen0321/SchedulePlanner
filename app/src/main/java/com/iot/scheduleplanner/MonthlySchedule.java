b55c05d175e905742c65dac68d63437ba18609bf
Younemuri <tomoy8371@gmail.com>
2022-07-15 16:36:36 +0900

    DailyPlanner

--- app/src/main/java/com/iot/scheduleplanner/MonthlySchedule.java
+++ app/src/main/java/com/iot/scheduleplanner/MonthlySchedule.java
@@ -3,11 +3,11 @@ package com.iot.scheduleplanner;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MonthlySchedule extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_monthly_schedule);
     }
 }
