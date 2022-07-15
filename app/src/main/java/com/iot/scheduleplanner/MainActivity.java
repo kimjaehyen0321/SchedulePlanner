0932a867326f8892de1c6001ab2a7819555fe6b9
Younemuri <tomoy8371@gmail.com>
2022-07-15 09:29:33 +0900

    setting file

---
+++ app/src/main/java/com/iot/scheduleplanner/MainActivity.java
@@ -0,0 +1,13 @@
+package com.iot.scheduleplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
