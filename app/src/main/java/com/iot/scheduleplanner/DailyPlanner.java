93c159bfd8baa04242f6a716c88056f21e39b386
Younemuri <tomoy8371@gmail.com>
2022-07-18 13:04:57 +0900

    complete

--- app/src/main/java/com/iot/scheduleplanner/DailyPlanner.java
+++ app/src/main/java/com/iot/scheduleplanner/DailyPlanner.java
@@ -1,13 +1,75 @@
 package com.iot.scheduleplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class DailyPlanner extends AppCompatActivity {
+    String text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_planner);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text = editText.getText().toString();
+
+                if(text != null)
+                    textView.setText(text);
+                editText.setText("");
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveState();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        restoreState();
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+
+        if(text != null)
+            textView.setText(text);
+    }
+
+    private void saveState() {
+        SharedPreferences pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("text", text);
+        editor.commit();
+    }
+
+    private void restoreState() {
+        SharedPreferences pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);
+        if((pref!=null) && (pref.contains("text"))){
+            text = pref.getString("text", "");
+        }
+    }
+
+    protected void clearPref() {
+        SharedPreferences pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.clear();
+        text = null;
+        editor.commit();
+
     }
 }
