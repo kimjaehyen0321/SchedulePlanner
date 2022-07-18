cf368c7d6114c0a81c7017ee5a190f1ec19d578d
Younemuri <tomoy8371@gmail.com>
2022-07-18 10:24:00 +0900

    complete

--- app/src/main/java/com/iot/scheduleplanner/MainActivity.java
+++ app/src/main/java/com/iot/scheduleplanner/MainActivity.java
@@ -1,7 +1,10 @@
 package com.iot.scheduleplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        Button button2 = (Button) findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MonthlySchedule.class);
+                startActivity(intent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DailyPlanner.class);
+                startActivity(intent);
+            }
+        });
     }
 }
